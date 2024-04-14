b17b47a97578a7dab289e75da93407f49785bc97
Viviam <vivian.lopesg@gmail.com>
2024-04-14 15:22:25 +0100

    created employees show page

--- client/src/router/routes.js
+++ client/src/router/routes.js
@@ -3,7 +3,8 @@ const routes = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', name: 'home', component: () => import('pages/IndexPage.vue') }
+      { path: '', name: 'home', component: () => import('pages/IndexPage.vue') },
+      { path: 'show-employee/:id', name: 'ShowEmployee', component: () => import('pages/employees/ShowEmployee.vue') }
     ]
   },
 
