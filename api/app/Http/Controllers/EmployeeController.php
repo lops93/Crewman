5bf3c36b14794c23f992bad02565f3ce2da2b941
Viviam <vivian.lopesg@gmail.com>
2024-04-14 01:31:55 +0100

    created employee methods and tests

--- api/app/Http/Controllers/EmployeeController.php
+++ api/app/Http/Controllers/EmployeeController.php
@@ -2,65 +2,36 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\EmployeeRequest;
 use App\Models\Employee;
-use Illuminate\Http\Request;
 
 class EmployeeController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
         $employees = Employee::all();
         return response()->json($employees);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
+    public function store(EmployeeRequest $request)
     {
-        //
+        Employee::create($request->validated());
+        return response()->json("Record created successfully");
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
     public function show(Employee $employee)
     {
         return response()->json($employee);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
+    public function update(EmployeeRequest $request, Employee $employee){
+        $employee->update($request->validated());
+        return response()->json("Record updated successfully");
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
+    public function destroy(Employee $employee)
     {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
+        $employee->delete();
+        return response()->json("Record deleted successfully");
     }
 }
